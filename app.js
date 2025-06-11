d947109af4a7a96433b89b7ca178d61bc8d7d391
Sandeep Lal <sandeep221102@gmail.com>
2025-06-11 15:41:02 +0530

    Subscription-Tracker initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { PORT } from "./config/env.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the subscription tracker api");
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `subscription Tracker API is running on http://localhost:${PORT}`
+  );
+});
+
+export default app;
